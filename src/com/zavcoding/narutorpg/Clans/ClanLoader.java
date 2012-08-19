44189cf318ba11e14842e27614a146c5ab1b2d5f
Cody Jackson <sasukeucheha@live.com>
2012-08-19 05:58:31 -0700

    Added a Command, Listener, and fixed stuff :3

--- src/com/zavcoding/narutorpg/Clans/ClanLoader.java
+++ src/com/zavcoding/narutorpg/Clans/ClanLoader.java
@@ -1,20 +1,70 @@
 package com.zavcoding.narutorpg.Clans;
 
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.zavcoding.narutorpg.NarutoRPG;
 
 public class ClanLoader {
-	
 	NarutoRPG plugin;
-	
-	ClanLoader(NarutoRPG instance) {
+
+	public ClanLoader(NarutoRPG instance) {
 		plugin = instance;
-	}
-	
+		}
+
+	@SuppressWarnings("resource")
 	public List<Clan> load() {
 		List<Clan> list = new ArrayList<Clan>();
+
+		// Check if folder exists
+		if (plugin.clanClassFolder == null) {
+			plugin.logger.warning("No clans found in the classes folder. This plugin will only use the default clans.");
+			return list;
+			}
+
+		// Create Class Loader Instance
+		ClassLoader loader;
+		try {
+			loader = new URLClassLoader(new URL[] { plugin.clanClassFolder.toURI().toURL() }, Clan.class.getClassLoader());
+			} catch (MalformedURLException ex) {
+				plugin.logger.warning("Clans have failed to load.");
+				return list;
+				}
+
+		// Load the class files
+		for (File f : plugin.clanClassFolder.listFiles()) {
+			// Make Sure to only load class files
+			if (!f.getName().endsWith(".class")) { 
+				continue;
+				}
+			String name = f.getName().substring(0,
+					f.getName().lastIndexOf("."));
+
+			try {
+				Class<?> clazz = loader.loadClass(name);
+				Object object = clazz.newInstance();
+				if (!(object instanceof Clan)) {
+					plugin.logger.info("This file is not a valid Clan file: " + clazz.getSimpleName());
+					continue;
+					}
+				Clan clan = (Clan) object;
+				clan.onEnable();
+				list.add(clan);
+				plugin.logger.info("Loaded clan: "+ list.getClass().getSimpleName());
+				/*
+				 * Used to catch any errors with loading
+				 * It will skip the clan
+                 */
+				} catch (Exception ex) {
+					plugin.logger.info("Error loading '" + name + "' clan! Clan disabled.");
+					} catch (Error ex) {
+						plugin.logger.info("Error loading '" + name + "' clan! Clan disabled.");
+						}
+			}
 		return list;
-	}
+		}
 }
--- src/com/zavcoding/narutorpg/NarutoRPG.java
+++ src/com/zavcoding/narutorpg/NarutoRPG.java
@@ -1,27 +1,45 @@
 package com.zavcoding.narutorpg;
 
 import java.io.File;
+import java.util.List;
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.zavcoding.narutorpg.Clans.Clan;
+import com.zavcoding.narutorpg.Clans.ClanLoader;
+import com.zavcoding.narutorpg.commands.NarutoRPGCommands;
 
 public class NarutoRPG extends JavaPlugin {
 
 	// Global Fields
 	public Logger logger;
-	public File clanFolder = new File(getDataFolder() + File.pathSeparator + "clans");
-	public File clanClassFolder = new File(clanFolder + File.pathSeparator + "classes");
+	public List<Clan> clans;
+	
+	public File clanFolder = new File("./plugins/NarutoRPG/Clans");
+	public File clanClassFolder = new File("./plugins/NarutoRPG/Classes");
+	public String prefix = "[" + ChatColor.DARK_RED + "NarutoRPG" + ChatColor.WHITE + "]";
 	
 	// Global Managers
+	ClanLoader clanLoader = new ClanLoader(this);
 	
 	@Override
 	public void onEnable() {
 		logger = getServer().getLogger();
 		checkFolders();
+		clans = clanLoader.load(); // Add clans to the list
+		getCommand("narutorpg").setExecutor(new NarutoRPGCommands(this));
+		Bukkit.getPluginManager().registerEvents(new NarutoRPGListener(this), this);
 		logger.info(this + " is now enabled.");
 		logger.info("This plugin is sponsored by BeastNode.com.");
 		logger.info("Create. Connect. Grow. Premium Hosting Services.");
-	}
+		if (clans.size() < 1) {
+			logger.severe("No clans are located in the clans folder. This plugin is shutting down");
+			//setEnabled(false);
+			}
+		}
 	
 	@Override
 	public void onDisable() {
