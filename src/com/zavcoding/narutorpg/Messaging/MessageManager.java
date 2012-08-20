8b1396cd15713644f8b89f4e16867ce7aaad9786
Zach Sugano <zachoooo@gmail.com>
2012-08-20 15:12:30 -0700

    Added clan messaging

--- src/com/zavcoding/narutorpg/Messaging/MessageManager.java
+++ src/com/zavcoding/narutorpg/Messaging/MessageManager.java
@@ -1,6 +1,10 @@
 package com.zavcoding.narutorpg.Messaging;
 
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
 import com.zavcoding.narutorpg.NarutoRPG;
+import com.zavcoding.narutorpg.Clans.Clan;
 
 public class MessageManager {
 	
@@ -12,6 +16,15 @@ public class MessageManager {
 	
 	public void sendMessage(String player, String s) {
 		plugin.getServer().getPlayer(player).sendMessage(plugin.prefix + s);
+	}
+	
+	public void sendClanMessage(Clan c, String s) {
+		for (OfflinePlayer op : c.getClanPlayers()) {
+			if (op.isOnline()) {
+				Player p = (Player) op;
+				p.sendMessage("[" + c.getName() + "] " + s);
+			}
+		}
 	}
 	
 	public void broadcastMessage(String s) {
