ed5f6f71f8e2e19ab30f1e6db85dfbb2080bcb9e
Zach Sugano <zachoooo@gmail.com>
2012-08-20 14:35:13 -0700

    Adding and removing players from a clan

--- src/com/zavcoding/narutorpg/Clans/Clan.java
+++ src/com/zavcoding/narutorpg/Clans/Clan.java
@@ -12,15 +12,15 @@ import com.zavcoding.narutorpg.NarutoRPG;
 
 public abstract class Clan {
 	private NarutoRPG plugin;
-	
+
 	private int currentChakra;
-	
+
 	private FileConfiguration config;
-	
+
 	public Clan(NarutoRPG instance) {
 		plugin = instance;
 		config = YamlConfiguration.loadConfiguration(new File(plugin.clanFolder, getName() + File.separator + "yml"));
-		}
+	}
 
 	public abstract String getName();
 
@@ -30,31 +30,49 @@ public abstract class Clan {
 
 	public NarutoRPG getPluginInstance() {
 		return plugin;
-		}
+	}
 
 	public FileConfiguration getConfig() {
 		return config;
-		}
+	}
 
 	public int getCurrentChakra() {
 		return currentChakra;
 	}
 	public void setCurrentChakra(int i) {
 		currentChakra = i;
-		}
+	}
 
 	public void changeCurrentChakraBy(int i) {
 		currentChakra += i;
+	}
+	
+	public void addPlayer(String s) {
+		List<OfflinePlayer> list = new ArrayList<OfflinePlayer>();
+		OfflinePlayer player = plugin.getServer().getOfflinePlayer(s);
+		if (player != null) {
+			list.add(plugin.getServer().getOfflinePlayer(s));
 		}
-
+		config.set("players", list);
+	}
+	
+	public void removePlayer(String s) {
+		List<OfflinePlayer> list = new ArrayList<OfflinePlayer>();
+		OfflinePlayer player = plugin.getServer().getOfflinePlayer(s);
+		if (list.contains(player.getName())) {
+			list.remove(player.getName());
+		}
+		config.set("players", list);
+	}
+	
 	public List<OfflinePlayer> getClanPlayers() {
 		List<OfflinePlayer> list = new ArrayList<OfflinePlayer>();
 		for (String s : config.getStringList("players")) {
 			OfflinePlayer player = plugin.getServer().getOfflinePlayer(s);
 			if (player != null) {
 				list.add(plugin.getServer().getOfflinePlayer(s));
-				}
 			}
-		return list;
 		}
+		return list;
 	}
+}
